ac30fbaf4e761b2d54dd50c69b60e111db9f745e
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-02-04 16:32:56 +0300

    first commit

---
+++ CSharp/Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] students = new string[3]{"Emre", "Merve", "Yusuf"};
+            //string[] students2 = {"Emre", "Merve", "Yusuf"};
+
+            //foreach (var student in students2)
+            //{
+            //    Console.WriteLine(student);
+            //}
+
+            //Console.ReadLine();
+            string[,] regions = new string[5, 3]
+            {
+                {"İstanbul", "Kocaeli", "Yalova"},
+                {"Ankara", "Konya", "Aksaray"},
+                {"Adana", "Antalya", "Mersin"},
+                {"Trabzon", "Rize", "Ordu"},
+                {"İzmir", "Muğla", "Manisa"}
+            };
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i, j]);
+                }
+                Console.WriteLine("------");
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ CSharp/Inheritance/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer()
+                {
+                    FirstName = "Kutluhan"
+                },
+                new Student()
+                {
+                    FirstName = "kutluhan"
+                },
+                new Person()
+                {
+                    FirstName = "Azafli"
+                }
+            };
+
+            foreach (var Person in persons)
+            {
+                Console.WriteLine(Person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+
+}
